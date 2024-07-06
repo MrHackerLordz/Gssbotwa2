2eaf49208188d7f651ac34d31cf5c435ec9a82cc
MrHackerLordz <165281290+MrHackerLordz@users.noreply.github.com>
2024-07-06 15:24:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ global.APIs = {
 
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = '𝑆𝛪𝐷 𝐵𝛨𝛥𝛪';
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = 'Mr Hacker Lordz';
+global.owner = ['2348184059232'];
+global.premium = ['2348184059232'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫';
+global.author = '🤖MrHackerLordz👾';
 global.sessionName = 'session';
 global.prefa = ['', '!', '.'];
 global.sp = '✪';
